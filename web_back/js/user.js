bdf4b9e273c438a12dccb663a3b5052eaa73859a
yutianzhengchong <337111893@qq.com>
2020-01-17 00:11:21 +0800

    最优

--- web_back/js/user.js
+++ web_back/js/user.js
@@ -3,41 +3,19 @@
 var user = {
     //退出
     tuichu: function () {
-        $.post(JKDIZHI.user_tuichu,
-        ).then(function (res) {
-            if (res.code === 200) {
-                window.location.href = "./login.html"
-            } else {
-                alert(res.msg)
-            }
-        })
+        return $.post(JKDIZHI.user_tuichu,
+        )
     },
     //管理员
     guanliyuan: function () {
-        $.get(JKDIZHI.user_guanliyuan,
-        ).then(function (res) {
-            if (res.code === 200) {
-                $('#yonghuming').text(res.data.nickname)
-                $('#img1').attr('src', res.data.user_pic)
-                $('#img2').attr('src', res.data.user_pic)
-            }
-        })
+        return $.get(JKDIZHI.user_guanliyuan)
     },
     //登录
     denglu: function (name, password) {
-        $.post(
+        return $.post(
             JKDIZHI.user_denglu, {
             'user_name': name,
             'password': password
-        }).then(function (res) {
-            // console.log(res);
-            if (res.code === 200) {
-                //成功跳转
-                window.location.href = "./index.html"
-            } else {
-                //失败报错
-                alert(res.msg)
-            }
         })
     }
 }
