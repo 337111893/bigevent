15f1b824480d344f527ea224c56e9b72033e6deb
yutianzhengchong <337111893@qq.com>
2020-02-04 22:08:46 +0800

    编辑完成

--- web_back/js/config.js
+++ web_back/js/config.js
@@ -1,9 +1,22 @@
 //设置地址优化
-var DIZHI = 'http://192.168.1.102:8000'
+var DIZHI = 'http://192.168.0.114:8000'
 
 var JKDIZHI = {
+    //登录
     user_denglu: DIZHI + '/admin/login',
+    //退出
     user_tuichu: DIZHI + '/admin/logout',
-    user_guanliyuan: DIZHI + '/admin/getuser'
+    //获取管理员
+    user_guanliyuan: DIZHI + '/admin/getuser',
+    //获取文章分类
+    category_get: DIZHI + '/admin/category_search',
+    //添加文章
+    category_add: DIZHI + '/admin/category_add',
+    //删除文章
+    category_del: DIZHI + '/admin/category_delete',
+    //编辑文章
+    category_edit: DIZHI + '/admin/category_edit'
+
+
 
 }
